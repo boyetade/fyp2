e657c053d769a5eb1f0adeeb23ea3c872be072f7
boyetade <44165788+boyetade@users.noreply.github.com>
2022-03-24 17:25:21 +0000

    small change

--- app.js
+++ app.js
@@ -24,5 +24,9 @@ app.set("view engine", "ejs")
 app.get('/', (req, res)=> {
    res.send('hi')
 })
+var client_id = 'bac3535f536b4a5c88dfe6d1fa5bcea9'; // Your client id
+var client_secret = 'cdbe500d9c164131928d1c9982fa33f0'; // Your secret
+var redirect_uri = 'http://localhost:3000/music.html'; // Your redirect uri
+var scopes = \'user-read-private user-read-email\'
 
 
